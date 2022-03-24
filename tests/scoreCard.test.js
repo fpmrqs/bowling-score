d3f4bc0a39a76d02290878f43320865541dfe6e5
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-03-24 14:45:40 +0000

    can roll again on tenth frame after strike

--- tests/scoreCard.test.js
+++ tests/scoreCard.test.js
@@ -1,5 +1,6 @@
 const ScoreCard = require("../lib/model/scoreCard");
 const Frame = require("../lib/model/frame");
+
 jest.mock("../lib/model/frame", () => {
   return jest.fn().mockImplementation(() => {
     return {
@@ -15,9 +16,43 @@ describe("ScoreCard class", () => {
        scoreCard = new ScoreCard(Frame);
      })
 
-  it("pins add to the total score", () => {
+  it("adds pins to the total score", () => {
     scoreCard.addKnockedPins(4);
     scoreCard.addKnockedPins(4);
     expect(scoreCard.getTotalPoints()).toEqual(8);
   });
+
+  describe("maximum pins", () => {
+    it("adds maximum of 10 pins", () => {
+      expect(() => scoreCard.addKnockedPins(11)).toThrow(
+        "this is a 10 pins bowling game!"
+      );
+    });
+  
+    it("adds maximum of 10 pins across two throws or a frame", () => {
+      scoreCard.addKnockedPins(6);
+      expect(() => scoreCard.addKnockedPins(6)).toThrow(
+        "this is a 10 pins bowling game!"
+      );
+    });
+  })
+
+  it("closes frame after strike", () => {
+    scoreCard.addKnockedPins(10);
+    scoreCard.addKnockedPins(5);
+    expect(scoreCard.frames.length).toEqual(2);
+  });
+
+  describe("tenth frame", () => {
+    it("strike on roll 1 lets you roll again", () => {
+      for (let i = 0; i < 18; i++) {
+        scoreCard.addKnockedPins(4);
+      }
+      scoreCard.addKnockedPins(10)
+      scoreCard.addKnockedPins(4)
+      expect(scoreCard.frames.length).toEqual(10);
+    });
+  })
+
+  
 });
