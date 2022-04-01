ac05f29333530eb7ad4e566716e7e23f7390a4ca
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-04-01 17:08:16 +0100

    DataOutput store Xs for tenth frame cases

--- tests/dataOutput.test.js
+++ tests/dataOutput.test.js
@@ -1,19 +1,37 @@
-const DataOutput = require("../lib/model/dataOutput")
+const DataOutput = require("../lib/model/dataOutput");
 
 describe("DataOutput class", () => {
-
   let output;
   beforeEach(() => {
-    output = new DataOutput;
+    output = new DataOutput();
   });
 
   it("stores rolls and returns them as strings", () => {
     output.addRoll(4);
     expect(output.getRoll(0)).toEqual("4");
-  })
+  });
 
   it("stores X for the second roll when strike", () => {
     output.addRoll(10);
     expect(output.getRoll(1)).toEqual("X");
-  })
-})
+  });
+  describe("tenth frame", () => {
+    it("stores X for the third roll", () => {
+      for (let i = 0; i <= 19; i++) {
+        output.addRoll(4);
+      }
+      expect(output.getRoll(20)).toEqual("X");
+    });
+
+    it("If double strike, X is not used for third roll", () => {
+      for (let i = 0; i <= 17; i++) {
+        output.addRoll(4);
+      }
+      output.addRoll(10);
+      output.addRoll(10);
+      output.addRoll(4);
+
+      expect(output.getRoll(20)).toEqual("4");
+    });
+  });
+});
